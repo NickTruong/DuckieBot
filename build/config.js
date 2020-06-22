9824243729c0289e85449c1ab1ba9b566ac988a8
Nick Truong <32306744+NickTruong@users.noreply.github.com>
2020-06-22 01:08:10 -0400

    Update config.js

--- build/config.js
+++ build/config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    "token": "NzI0NDIwNTExMzE2MzEyMTQ2.XvARQg.aeZdDRz3Eq1vtUAK-VFSvBp5PEQ",
+    "token": "-----------------------",
     "prefix": ";"
 };
