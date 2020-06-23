8af7f7401d50d590d6bd84738fac9e513899e230
Nick Truong <nicktruong@outlook.com>
2020-06-23 16:54:32 -0400

    Adding a .gitignore. Removing unnecessary stuff.

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    "token": "NzI0NDIwNTExMzE2MzEyMTQ2.XvBg4g.HCzj_HVUKq9UvX9Og6_r9Cki3rw",
-    "prefix": ";"
-};
